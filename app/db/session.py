f2e00b2a00486111927cff273e584eeca1b700d9
DmitryTL <Dmitry.plus1@yandex.ru>
2025-12-24 16:19:15 +0600

    fix configuring the DB

--- app/db/session.py
+++ app/db/session.py
@@ -1,14 +1,7 @@
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from app.core.config import settings
 
-engine = create_async_engine(settings.DATABASE_URL, echo=True, connect_args={"check_same_thread": False})
-
-SessionLocal = sessionmaker(
-    bind=engine,
-    class_=AsyncSession,
-    expire_on_commit=False,
-    autoflush=False,
-    autocommit=False)
-
+engine = create_engine(settings.DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 Base = declarative_base()
